d882e86d669e91d6c7f50700742094c34f75ff59
Andre Brasil <brasil.a@protonmail.com>
2022-02-18 15:25:16 +0000

    Add a dedicated file for the App class

---
+++ wmenu/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace wmenu
+{
+    public class App : IComparable
+    {
+        public string name;
+        public string path;
+
+        public int CompareTo(object obj)
+        {
+            return string.Compare(this.name, ((App)obj).name);
+        }
+    }
+}
